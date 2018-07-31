79e5b77de0bc0a2dd85770ea5c502357d8088d32
Ivan <porter190@yandex.ru>
2018-07-31 17:29:34 +0300

    Add files via upload

---
+++ newServer.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const fs = require('fs');
+const sevr = http.createServer(function(request,response){
+    console.log(request.method, request.url);
+    const file = fs.readFileSync('Just1.html','utf8');
+    response.end(file);
+});
+sevr.listen(3000);
+console.log('Server started!!');
